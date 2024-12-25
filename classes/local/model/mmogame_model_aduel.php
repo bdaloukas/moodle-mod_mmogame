a48be16de2013aae8102ea2c9853cb9d9410657b
root <bdaloukas@gmail.com>
2024-12-25 23:30:09 +0200

    Should be lowercase English words, separated by underscores

---
+++ classes/local/model/mmogame_model_aduel.php
@@ -0,0 +1,224 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the model ADuel
+ *
+ * @package    mod_mmogame
+ * @copyright  2024 Vasilis Daloukas
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_mmogame\local\model;
+
+use mod_mmogame\local\mmogame;
+
+/**
+ * The class mmogame_model_aduel has the code for model ADuel
+ *
+ * @package    mod_mmogame
+ * @copyright  2024 Vasilis Daloukas
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mmogame_model_aduel {
+
+    /**
+     * Administrator can change numgame or state
+     *
+     * @param object $data
+     * @param mmogame $game
+     */
+    public static function json_setadmin(object $data, mmogame $game) {
+        if (isset( $data->numgame) && $data->numgame > 0) {
+            $game->get_rstate()->state = 0;
+            $game->get_db()->update_record( 'mmogame', ['id' => $game->get_id(), 'numgame' => $data->numgame]);
+            $game->update_state( $game->get_rstate()->state);
+            $game->set_state( $game->get_rstate()->state);
+        } else if (isset( $data->state)) {
+            if ($data->state >= 0 && $data->state <= STATE_LAST) {
+                $game->update_state( $data->state);
+                $game->set_state( $data->state);
+            }
+        }
+    }
+
+    /**
+     * Return the aduel record for current $mmogame record
+     *
+     * @param mmogame $mmogame
+     * @param int $maxalone
+     * @param bool $newplayer1
+     * @param bool $newplayer2
+     * @return false|mixed
+     */
+    public static function get_aduel(mmogame $mmogame, int $maxalone, bool &$newplayer1, bool &$newplayer2) {
+        $newplayer1 = $newplayer2 = false;
+        $auserid = $mmogame->get_auserid();
+        $db = $mmogame->get_db();
+
+        $stat = $db->get_record_select( 'mmogame_aa_stats', 'mmogameid=? AND numgame=? AND auserid=? AND queryid IS NULL',
+            [$mmogame->get_id(), $mmogame->get_numgame(), $auserid]);
+        if ($stat === false) {
+            $stat = new \stdClass();
+            $stat->percent = $stat->id = $stat->count1 = $stat->count2 = 0;
+        }
+
+        // Returns one that is started and not finished.
+        $recs = $db->get_records_select( 'mmogame_am_aduel_pairs',
+            'mmogameid=? AND numgame=? AND '.
+            '(auserid1=? AND timestart1 <> 0 AND isclosed1 = 0 OR auserid2=? AND timestart2 <> 0 AND isclosed2 = 0)',
+            [$mmogame->get_id(), $mmogame->get_numgame(), $auserid, $auserid], 'id', '*', 0, 1);
+        $rec = false;
+        foreach ($recs as $rec) {
+            return $rec;
+        }
+        // Count1=count alone, Count2=count with an opposite.
+        if ($stat->count1 <= $stat->count2) {
+            return self::get_aduel_new( $mmogame, $newplayer1, $stat);
+        }
+
+        // Count1 is bigger than count2.
+
+        // Selects one of the games without opponent.
+        $sql = "SELECT a.*, s.percent".
+            " FROM {mmogame_am_aduel_pairs} a ".
+            " LEFT JOIN {mmogame_aa_stats} s ON ".
+            "a.mmogameid=s.mmogameid AND a.numgame=s.numgame AND a.auserid1=s.auserid AND s.queryid IS NULL AND numteam IS NULL".
+            " WHERE a.auserid2 IS NULL AND a.mmogameid=? AND a.numgame=? AND a.auserid1<>? AND a.isclosed1 = 1";
+        $recs = $db->get_records_sql( $sql, [$mmogame->get_id(), $mmogame->get_numgame(), $auserid]);
+
+        if ($stat->count1 - $stat->count2 == 1) {
+            if (count( $recs) < 3) {
+                return self::get_aduel_new( $mmogame, $newplayer1, $stat);
+            }
+        }
+
+        if (count( $recs) == 0) {
+            $count = $db->count_records_select( 'mmogame_am_aduel_pairs',
+                'mmogameid=? AND numgame=? AND auserid1 = ? AND auserid2 IS NULL',
+                [$mmogame->get_id(), $mmogame->get_numgame(), $auserid]);
+            if ($count > $maxalone) {
+                return false;   // Wait an opponent.
+            }
+            return self::get_aduel_new( $mmogame, $newplayer1, $stat);
+        }
+
+        // There are many "alone" games.
+        // Find a game with percent near my percent.
+        $map = [];    // The map1 contains games with lower grade and map2 with upper grade.
+        foreach ($recs as $rec) {
+            $step = $rec->percent <= $stat->percent ? 1 : 2; // 1 mean lower than my percent.
+            // Try to find the bigger of smaller or small of bigger percent.
+            $key = $step.sprintf( '%10.6f %10d', abs( $rec->percent - $stat->percent), $rec->id);
+            $map[$key] = $rec;
+        }
+        ksort( $map);
+
+        foreach ($map as $rec) {
+            break;
+        }
+
+        // Check if it has a game without opponent.
+        $rec->auserid2 = $auserid;
+        $rec->timestart2 = time();
+        $db->update_record( 'mmogame_am_aduel_pairs', ['id' => $rec->id, 'auserid2' => $auserid, 'timestart2' => time()]);
+        $newplayer2 = true;
+        if ($stat->id == 0) {
+            $mmogame->get_qbank()->update_stats( $mmogame->get_auserid(), null, null, 0, 0, 0, ['count2' => 1]);
+        } else {
+            $db->update_record( 'mmogame_aa_stats', ['id' => $stat->id, 'count2' => $stat->count2 + 1]);
+        }
+        return $rec;
+    }
+
+    /**
+     * Return the new aduel record for current $mmogame
+     *
+     * @param mmogame $mmogame
+     * @param bool $newplayer1
+     * @param object $stat (the record of table mmogame_aa_stats)
+     * @return mixed
+     */
+    public static function get_aduel_new(mmogame $mmogame, bool &$newplayer1, object $stat) {
+        $db = $mmogame->get_db();
+
+        $a = ['mmogameid' => $mmogame->get_id(), 'numgame' => $mmogame->get_numgame(), 'auserid1' => $mmogame->get_auserid(),
+            'timestart1' => time(), 'timelimit' => $mmogame->get_timelimit(), 'isclosed1' => 0, 'isclosed2' => 0, ];
+        $id = $db->insert_record( 'mmogame_am_aduel_pairs', $a);
+
+        $newplayer1 = true;
+        if ($stat->id == 0) {
+            $mmogame->get_qbank()->update_stats( $mmogame->get_auserid(), 0, 0, 0, 0, 0, ['count1' => 1]);
+        } else {
+            $db->update_record( 'mmogame_aa_stats', ['id' => $stat->id, 'count1' => $stat->count1 + 1]);
+        }
+
+        return $db->get_record_select( 'mmogame_am_aduel_pairs', 'id=?', [$id]);
+    }
+
+    /**
+     * Return an attempt record of the game
+     *
+     * @param mmogame $mmogame
+     * @param object $aduel
+     * @return false|object (the attempt record)
+     */
+    public static function get_attempt(mmogame $mmogame, object $aduel) {
+
+        $table = $mmogame->get_table_attempts();
+        $db = $mmogame->get_db();
+
+        $recs = $mmogame->get_db()->get_records_select( $table, "auserid=? AND numgame=? AND numteam=? AND timeanswer=0",
+            [$mmogame->get_auserid(), $mmogame->get_numgame(), $aduel->id], 'numattempt');
+        $time = time();
+
+        foreach ($recs as $rec) {
+            if ($rec->timeclose > $time || $rec->timeclose == 0) {
+                if ($rec->timestart == 0) {
+                    $rec->timestart = time();
+                    $rec->timeclose = $rec->timestart + $aduel->timelimit;
+                    $db->update_record( $table, ['id' => $rec->id, 'timestart' => $rec->timestart, 'timeclose' => $rec->timeclose]);
+                }
+                return $rec;
+            }
+            if ($rec->timestart == 0) {
+                $rec->timestart = $time;
+                $rec->timeclose = $time + $aduel->timelimit;
+                $db->update_record( $table, ['id' => $rec->id, 'timestart' => $rec->timestart, 'timeclose' => $rec->timeclose]);
+                return $rec;
+            }
+        }
+
+        $a = ['id' => $aduel->id];
+        if ($mmogame->get_auserid() == $aduel->auserid1) {
+            $a['isclosed1'] = 1;
+        } else {
+            $a['isclosed2'] = 1;
+        }
+        $mmogame->get_db()->update_record( 'mmogame_am_aduel_pairs', $a);
+
+        return false;
+    }
+
+    /**
+     * Deletes all pairs (table mmogame_am_aduel_pairs)
+     *
+     * @param mmogame $mmogame
+     */
+    public static function delete(mmogame $mmogame) {
+        $mmogame->get_db()->delete_records_select( 'mmogame_am_aduel_pairs', 'mmogameid=?', [$mmogame->get_id()]);
+    }
+}
---
+++ classes/local/qbank/mmogame_qbank.php
@@ -0,0 +1,292 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * lib
+ *
+ * @package    mod_mmogame
+ * @copyright  2024 Vasilis Daloukas
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_mmogame\local\qbank;
+use mod_mmogame;
+use mod_mmogame\local\mmogame;
+
+/**
+ * The class mmogame_qbank is a based class for saved questions (e.g., glossary, question bank)
+ *
+ * @package    mmogame_qbank_moodlequestion
+ * @copyright  2024 Vasilis Daloukas
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mmogame_qbank {
+    /** @var mod_mmogame\local\mmogame: the object mmogame that is connected to the question bank. */
+    protected mod_mmogame\local\mmogame $mmogame;
+
+    /**
+     * Constructor.
+     *
+     * @param mmogame $mmogame
+     */
+    public function __construct(mmogame $mmogame) {
+        $this->mmogame = $mmogame;
+    }
+
+    /**
+     * The base function for a new attempt.
+     *
+     * @param int $count
+     * @param bool $usenumattempt
+     * @param array|null $queries
+     * @return array|false
+     */
+    public function get_attempt_new(int $count, bool $usenumattempt, ?array $queries) {
+        $db = $this->mmogame->get_db();
+        $rgame = $this->mmogame->get_rgame();
+        $auserid = $this->mmogame->get_auserid();
+
+        if ($queries === null) {
+            $queries = $this->get_queries( $auserid, 0, $count);
+            if ($queries === false) {
+                return false;
+            }
+        }
+
+        if ($usenumattempt) {
+            $rec = $db->get_record_select( $this->mmogame->get_table_attempts(), 'mmogameid=? AND numgame=? AND auserid=?',
+                [$rgame->id, $rgame->numgame, $auserid], 'max(numattempt) as num');
+            $numattempt = $rec->num + 1;
+        }
+
+        $a = [];
+
+        $a['mmogameid'] = $rgame->id;
+        $a['numgame'] = $rgame->numgame;
+        $a['auserid'] = $auserid;
+        if ($usenumattempt) {
+            $a['numattempt'] = $numattempt;
+        }
+        $a['timeclose'] = 0;
+        if ($count == 1) {
+            $a['queryid'] = $queries[0]->id;
+            $a['layout'] = $this->get_layout( $queries[0]);
+        }
+        $a['queries'] = $queries;
+        $a['useranswerid'] = 0;
+        $a['useranswer'] = '';
+        $a['timeanswer'] = 0;
+        $a['iscorrect'] = -1;
+
+        return $a;
+    }
+
+    /**
+     * Returns the id of selected queries.
+     *
+     * @param int $auserid
+     * @param int $numteam
+     * @param int $count (how many queries they want)
+     *
+     * @return false|array of int or false if no queries found.
+     */
+    protected function get_queries(int $auserid, int $numteam, int $count) {
+        $ids = $this->get_queries_ids();
+        if ($ids === false) {
+            return false;
+        }
+
+        $db = $this->mmogame->get_db();
+        $rgame = $this->mmogame->get_rgame();
+
+        if ($numteam == 0) {
+            $stats = $db->get_records_select( 'mmogame_aa_stats', 'mmogameid=? AND numgame=? AND auserid=?',
+                [$rgame->id, $rgame->numgame, $auserid], '', 'queryid,countused,countcorrect,counterror');
+        } else {
+            $stats = $db->get_records_select( 'mmogame_aa_stats', 'mmogameid=? AND numgame=? AND numteam=?',
+                [$rgame->id, $rgame->numgame, $numteam], '', 'queryid,countused,countcorrect,counterror');
+        }
+
+        $map = [];
+        foreach ($ids as $id => $qtype) {
+            $r = mt_rand( 0, 1000 * 1000 - 1);
+            $stat = array_key_exists( $id, $stats) ? $stats[$id] : false;
+            $group = 0;
+
+            $countused = $stat ? $stat->countused : 0;
+            $countcorrect = $stat !== false ? $stat->countcorrect : 0;
+            $key = sprintf( "%10d-%10d-%10d-%10d-%10d", $countcorrect, $countused, $group, $r, $id);
+            $map[$key] = $id;
+        }
+        ksort( $map);
+
+        $ret = [];
+        foreach ($map as $id) {
+            if (count( $ret) >= $count) {
+                break;
+            }
+            $q = $this->load( $id);
+            $ret[] = $q;
+        }
+        shuffle( $ret);
+
+        foreach ($ret as $q) {
+            if ($numteam != null && $numteam != 0) {
+                $this->update_stats( 0, $numteam, $q->id, true, false, false);
+            } else {
+                $this->update_stats( $auserid, 0, $q->id, true, false, false);
+            }
+        }
+
+        return count( $ret) ? $ret : false;
+    }
+
+    /**
+     * Updates the grade in database (table mmogame_aa_grades).
+     *
+     * The score2 is a temporary score e.g. chat phase of arguegraph.
+     *
+     * @param int $auserid
+     * @param int $score
+     * @param int $score2
+     * @param int $countscore (how to increase the score)
+     */
+    public function update_grades(int $auserid, int $score, int $score2, int $countscore): void {
+        $db = $this->mmogame->get_db();
+        $rgame = $this->mmogame->get_rgame();
+        $rec = $db->get_record_select( 'mmogame_aa_grades', 'mmogameid=? AND numgame=? AND auserid=?',
+                [$rgame->id, $rgame->numgame, $auserid]);
+        if ($rec === false) {
+            $this->mmogame->get_grade( $auserid);
+            $rec = $db->get_record_select( 'mmogame_aa_grades', 'mmogameid=? AND numgame=? AND auserid=?',
+                [$rgame->id, $rgame->numgame, $auserid]);
+        }
+        if ($rec !== false) {
+            $a = ['id' => $rec->id];
+            if ($countscore > 0) {
+                $a['countscore'] = $rec->countscore + $countscore;
+                $a['score'] = max( 0, $rec->sumscore + $score) / ($rec->countscore + $countscore);
+            }
+            if ($score != 0) {
+                $a['sumscore'] = max( 0, $rec->sumscore + $score);
+            }
+            if ($score2 != 0) {
+                $a['sumscore2'] = max( 0, $rec->sumscore2 + $score2);
+            }
+            if (count( $a) > 1) {
+                $db->update_record( 'mmogame_aa_grades', $a);
+            }
+        } else {
+            $db->insert_record( 'mmogame_aa_grades',
+                ['mmogameid' => $rgame->id, 'numgame' => $rgame->numgame, 'auserid' => $auserid, 'sumscore' => max( 0, $score),
+                'countscore' => $countscore,
+                'score' => max( 0, $score), 'sumscore2' => max( 0, $score2), 'timemodified' => time(), ]);
+        }
+    }
+
+    /**
+     * Updates statistics in the database (table mmogame_aa_stats).
+     *
+     * The score2 is a temporary score e.g. chat phase of arguegraph.
+     *
+     * @param ?int $auserid
+     * @param ?int $numteam
+     * @param ?int $queryid
+     * @param bool $isused
+     * @param bool $iscorrect
+     * @param bool $iserror
+     * @param array|null $values
+     */
+    public function update_stats(?int $auserid, ?int $numteam, ?int $queryid, bool $isused, bool $iscorrect, bool $iserror,
+        ?array $values = null) {
+
+        $db = $this->mmogame->get_db();
+        $rgame = $this->mmogame->get_rgame();
+
+        $select = 'mmogameid=? AND numgame=? ';
+        $a = [$rgame->id, $rgame->numgame];
+        if ($auserid != 0) {
+            $select .= ' AND auserid=?';
+            $a[] = $auserid;
+        } else {
+            $select .= ' AND auserid IS NULL';
+            $auserid = null;
+        }
+        if ($numteam != 0) {
+            $select .= ' AND numteam=?';
+            $a[] = $numteam;
+        } else {
+            $select .= ' AND numteam IS NULL';
+            $numteam = null;
+        }
+        if ($queryid != 0) {
+            $select .= ' AND queryid=?';
+            $a[] = $queryid;
+        } else {
+            $select .= ' AND queryid IS NULL';
+            $queryid = 0;
+        }
+
+        $rec = $db->get_record_select( 'mmogame_aa_stats', $select, $a);
+        if ($rec !== false) {
+            $a = ['id' => $rec->id];
+            if ($isused > 0) {
+                $a['countused'] = $rec->countused + $isused;
+            }
+            if ($iscorrect > 0) {
+                $rec->countcorrect += $iscorrect;
+                $a['countcorrect'] = $rec->countcorrect;
+            }
+            if ($iserror > 0) {
+                $rec->counterror += $iserror;
+                $a['counterror'] = $rec->counterror;
+                $a['timeerror'] = time();
+            }
+
+            $count = $rec->countcorrect + $rec->counterror;
+            $a['percent'] = $count == 0 ? null : $rec->countcorrect / $count;
+            if ($values !== null) {
+                foreach ($values as $key => $value) {
+                    if ($value == 'percent2') {
+                        $value /= $rec->countanswers;
+                    }
+                    $a[$key] = $value;
+                }
+                if ($queryid == null && array_key_exists( 'countcompleted', $values)) {
+                    $n = $rec->countanswers;
+                    $percentcompleted = $n != 0 ? $values['countcompleted'] / $n : 0;
+                    $sql = "UPDATE {mmogame_aa_grades} ".
+                        "SET percentcompleted=? WHERE mmogameid=? AND numgame=? AND auserid=?";
+                    $db->execute( $sql, [$percentcompleted, $rgame->id, $rgame->numgame, $auserid]);
+                }
+            }
+            $db->update_record( 'mmogame_aa_stats', $a);
+        } else {
+            $count = $iscorrect + $iserror;
+            $percent = $count == 0 ? null : $iscorrect / $count;
+            $a = ['mmogameid' => $rgame->id,
+                'numgame' => $rgame->numgame, 'queryid' => $queryid != 0 ? $queryid : null, 'auserid' => $auserid,
+                'numteam' => $numteam != 0 ? $numteam : null, 'percent' => $percent, 'countused' => $isused,
+                'countcorrect' => $iscorrect, 'counterror' => $iserror, ];
+            if ($values !== null) {
+                foreach ($values as $key => $value) {
+                    $a[$key] = $value;
+                }
+            }
+            $db->insert_record( 'mmogame_aa_stats', $a);
+        }
+    }
+}
