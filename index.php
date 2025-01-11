96f983ec1b9a6e1386b1d0856a80cf7ceb40000c
Vasilis Daloukas <bdaloukas@gmail.com>
2025-01-11 17:57:31 +0200

    New index.php show the list of mmogames

--- index.php
+++ index.php
@@ -15,110 +15,60 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * MMOGame class.
+ * Index file for the mmogame module.
  *
  * @package    mod_mmogame
- * @copyright  2024 Vasilis Daloukas
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * * @copyright  2024 Vasilis Daloukas
+ * * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-use mod_mmogame\event\course_module_instance_list_viewed;
+require_once(__DIR__ . '/../../config.php');
 
-require_once("../../config.php");
-require_once("lib.php");
+$id = required_param('id', PARAM_INT); // Course ID.
 
-$id = required_param('id', PARAM_INT);
+// Verify the course exists.
+if (!$course = $DB->get_record('course', ['id' => $id])) {
+    throw new moodle_exception('invalidcourseid', 'error');
+}
 
-$PAGE->set_url('/mod/mmogame/index.php', ['id' => $id]);
-$course = get_course($id);
-$coursecontext = context_course::instance($id);
+// Require login to the course.
 require_login($course);
-$PAGE->set_pagelayout('incourse');
 
-$params = ['context' => $coursecontext];
-$event = course_module_instance_list_viewed::create($params);
-$event->trigger();
+$context = context_course::instance($course->id);
+require_capability('mod/mmogame:view', $context);
 
-// Print the header.
-$strmmogames = get_string("modulenameplural", "mmogame");
-$PAGE->navbar->add($strmmogames);
-$PAGE->set_title($strmmogames);
+// Set up the page.
+$PAGE->set_url('/mod/mmogame/index.php', ['id' => $id]);
+$PAGE->set_title(get_string('modulenameplural', 'mod_mmogame'));
 $PAGE->set_heading($course->fullname);
-/** @var renderer $output */
-$output = $PAGE->get_renderer('mod_mmogame');
-echo $output->header();
-echo $output->heading($strmmogames, 2);
 
-// Get all the appropriate data.
-if (!$mmogames = get_all_instances_in_course("mmogame", $course)) {
-    notice(get_string('thereareno', 'moodle', $strmmogames), "../../course/view.php?id=$course->id");
-    die;
-}
+// Get all instances of the module in the course.
+$instances = get_all_instances_in_course('mmogame', $course);
 
-// Configure table for displaying the list of instances.
-$headings = [get_string('name')];
-$align = ['left', 'left'];
+echo $OUTPUT->header();
 
-if (course_format_uses_sections($course->format)) {
-    array_unshift($headings, get_string('sectionname', 'format_'.$course->format));
+// Display the module list.
+if (!$instances) {
+    echo $OUTPUT->notification(get_string('noinstances', 'mod_mmogame'));
 } else {
-    array_unshift($headings, '');
+    $table = new html_table();
+    $table->head = [
+        get_string('name'),
+        get_string('introduction', 'mod_mmogame'),
+    ];
+
+    foreach ($instances as $instance) {
+        $link = html_writer::link(
+            new moodle_url('/mod/mmogame/view.php', ['id' => $instance->coursemodule]),
+            format_string($instance->name)
+        );
+
+        $intro = format_module_intro('mmogame', $instance, $instance->coursemodule);
+
+        $table->data[] = [$link, $intro];
+    }
+
+    echo html_writer::table($table);
 }
-array_unshift($align, 'center');
 
-$showing = '';
-
-if (has_capability('mod/mmogame:viewreports', $coursecontext)) {
-    array_push($headings, get_string('attempts', 'mmogame'));
-    array_push($align, 'left');
-    $showing = 'stats';
-}
-
-$table = new html_table();
-$table->head = $headings;
-$table->align = $align;
-
-// Populate the table with the list of instances.
-$currentsection = '';
-
-foreach ($mmogames as $mmogame) {
-    $cm = get_coursemodule_from_instance('mmogame', $mmogame->id);
-    $context = context_module::instance($cm->id);
-    $data = [];
-
-    // Section number if necessary.
-    $strsection = '';
-    if ($mmogame->section != $currentsection) {
-        if ($mmogame->section) {
-            $strsection = $mmogame->section;
-            $strsection = get_section_name($course, $mmogame->section);
-        }
-        if ($currentsection !== "") {
-            $table->data[] = 'hr';
-        }
-        $currentsection = $mmogame->section;
-    }
-    $data[] = $strsection;
-
-    // Link to the instance.
-    $class = '';
-    if (!$mmogame->visible) {
-        $class = ' class="dimmed"';
-    }
-    $data[] = "<a$class href=\"view.php?id=$mmogame->coursemodule\">" .
-        format_string($mmogame->name, true) . '</a>';
-
-    if ($showing == 'stats') {
-        // The $mmogame objects returned by get_all_instances_in_course have the necessary $cm
-        // fields set to make the following call work.
-        $data[] = \mmogame_attempt_summary_link_to_reports($mmogame, $cm, $context);
-    }
-
-    $table->data[] = $data;
-} // End of loop over mmogame instances.
-
-// Display the table.
-echo html_writer::table($table);
-
-// Finish the page.
-echo $output->footer();
+echo $OUTPUT->footer();
