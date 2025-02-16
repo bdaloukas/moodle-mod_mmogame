0dc7fbea37f852c799bbb6e8a35e618ea87b9867
Vasilis Daloukas <bdaloukas@gmail.com>
2025-02-16 17:25:10 +0200

    New: Use of events for logs

---
+++ classes/event/course_module_played.php
@@ -0,0 +1,84 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The mod_mmogame course module viewed event.
+ *
+ * @package    mod_mmogame
+ * @copyright  2024 Vasilis Daloukas
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_mmogame\event;
+
+use coding_exception;
+use context_module;
+use stdClass;
+
+/**
+ * The mod_mmogame course module viewed event class.
+ */
+class course_module_played extends \core\event\course_module_viewed {
+
+    /**
+     * Create instance of event.
+     *
+     * @param stdClass $mmogame
+     * @param context_module $context
+     *
+     * @return \core\event\base
+     * @throws coding_exception
+     */
+    public static function played(stdClass $mmogame, context_module $context): \core\event\base {
+        $data = ['context' => $context, 'objectid' => $mmogame->id];
+        $event = self::create($data);
+        $event->add_record_snapshot('mmogame', $mmogame);
+
+        return $event;
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
+    public static function get_name(): string {
+        return get_string('eventcoursemoduleplayed', 'mod_mmogame');
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "The user with id '$this->userid' played the '{$this->objecttable}' activity with " .
+            "course module id '$this->contextinstanceid'.";
+    }
+
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['crud'] = 'r';
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+        $this->data['objecttable'] = 'mmogame';
+    }
+}
